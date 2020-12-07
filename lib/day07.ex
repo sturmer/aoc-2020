533954a4d5441501ae94678e7f47757780250e46
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2020-12-07 09:03:52 +0200

    Do day 7

--- lib/day07.ex
+++ lib/day07.ex
@@ -1,19 +1,94 @@
 defmodule Aoc2020.Day07 do
   def solve1 do
-    # parse_and_solve(&Aoc2020.Day07.count_answers/1)
+    parse_and_solve(&Aoc2020.Day07.count_paths/1)
   end
 
   def solve2 do
-    # parse_and_solve(&Aoc2020.Day07.count_agreements/1)
+    parse_and_solve(&Aoc2020.Day07.count_bags/1)
   end
 
   defp parse_and_solve(fun) do
     case File.read("day7.input.txt") do
       {:ok, content} ->
-        String.split(content, "\n\n", trim: true) |> fun.() |> IO.puts()
+        String.split(content, "\n", trim: true) |> fun.() |> IO.puts()
 
       {:error, _} ->
         IO.puts("Error reading file")
     end
+  end
+
+  def count_bags(rules) do
+    graph = build_map(rules)
+    count_children("shiny gold", graph)
+  end
+
+  def count_children(src, gr) do
+    children = Map.get(gr, src)
+
+    if children == nil do
+      0
+    else
+      Enum.reduce(Map.keys(children), 0, fn child, acc ->
+        multiplier = Map.get(children, child)
+
+        # need to count the containing bags too!
+        acc + multiplier * count_children(child, gr) + multiplier
+      end)
+    end
+  end
+
+  def count_paths(rules) do
+    graph = build_map(rules)
+
+    Enum.reduce(Map.keys(graph), 0, fn src, acc ->
+      if can_reach(src, "shiny gold", graph) do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
+
+  def can_reach(src, dest, gr) do
+    children = Map.get(gr, src)
+
+    if children == nil do
+      false
+    else
+      if Map.has_key?(children, dest) do
+        true
+      else
+        Enum.any?(Map.keys(children), fn child -> can_reach(child, dest, gr) end)
+      end
+    end
+  end
+
+  def build_map(rules) do
+    Enum.reduce(rules, %{}, fn rule, acc ->
+      [source, sinks] = String.split(rule, "contain", trim: true)
+      stripped_source = strip_source(source)
+      stripped_sinks = strip_sinks(sinks)
+
+      Map.put(acc, stripped_source, stripped_sinks)
+    end)
+  end
+
+  def strip_source(s) do
+    # "wavy green bags "
+    String.split(s, "bag", parts: 2) |> Enum.at(0) |> String.trim()
+  end
+
+  def strip_sinks(sinks) do
+    # " 1 posh black bag, 1 faded green bag, 4 wavy red bags."
+    String.trim(sinks)
+    |> String.split(", ")
+    |> Enum.map(fn s -> String.split(s, " ", parts: 2) end)
+    |> Enum.reduce(%{}, fn sink, acc ->
+      [num, desc] = sink
+
+      if num != "no" do
+        Map.put(acc, strip_source(desc), String.to_integer(num))
+      end
+    end)
   end
 end
--- test/day07_test.exs
+++ test/day07_test.exs
@@ -3,6 +3,74 @@ defmodule Day07Test do
   doctest Aoc2020.Day07
   import Aoc2020.Day07
 
-  test "..." do
+  test "strip sinks" do
+    sinks = " 1 posh black bag, 1 faded green bag, 4 wavy red bags."
+    m = strip_sinks(sinks)
+    assert Map.get(m, "posh black") == 1
+    assert Map.get(m, "faded green") == 1
+    assert Map.get(m, "wavy red") == 4
+  end
+
+  test "part 1" do
+    input = """
+    light red bags contain 1 bright white bag, 2 muted yellow bags.
+    dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+    bright white bags contain 1 shiny gold bag.
+    muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+    shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+    dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+    faded blue bags contain no other bags.
+    dotted black bags contain no other bags.
+    """
+
+    assert String.split(input, "\n", trim: true) |> count_paths() == 4
+  end
+
+  test "part 2 second example" do
+    input = """
+    shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags.
+    """
+
+    assert String.split(input, "\n", trim: true) |> count_bags() == 126
+  end
+
+  test "part 2 first example" do
+    input = """
+    light red bags contain 1 bright white bag, 2 muted yellow bags.
+    dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+    bright white bags contain 1 shiny gold bag.
+    muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+    shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+    dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+    faded blue bags contain no other bags.
+    dotted black bags contain no other bags.
+    """
+
+    assert String.split(input, "\n", trim: true) |> count_bags() == 32
+  end
+
+  @tag :skip
+  test "count children" do
+    graph = %{
+      "bright white" => %{"shiny gold" => 1},
+      "dark olive" => %{"dotted black" => 4, "faded blue" => 3},
+      "dark orange" => %{"bright white" => 3, "muted yellow" => 4},
+      "dotted black" => nil,
+      "faded blue" => nil,
+      "light red" => %{"bright white" => 1, "muted yellow" => 2},
+      "muted yellow" => %{"faded blue" => 9, "shiny gold" => 2},
+      "shiny gold" => %{"dark olive" => 1, "vibrant plum" => 2},
+      "vibrant plum" => %{"dotted black" => 6, "faded blue" => 5}
+    }
+
+    assert count_children("shiny gold", graph) == 29
   end
 end
