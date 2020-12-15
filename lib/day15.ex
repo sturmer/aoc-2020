15ee1c6a294bf3ca9d8c892024c02cd1a06c5e2b
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2020-12-15 16:45:22 +0200

    Solve part 2 (with the same code, albeit inefficient)

--- lib/day15.ex
+++ lib/day15.ex
@@ -1,30 +1,22 @@
-defmodule Aoc2020.Day15.Part1 do
-  def solve() do
-    execute("7,14,0,17,11,1,2")
+defmodule Aoc2020.Day15 do
+  def solve(target \\ 2020) do
+    execute("7,14,0,17,11,1,2", target)
   end
 
   @doc """
-      iex> Aoc2020.Day15.Part1.execute("0,3,6")
+      iex> Aoc2020.Day15.execute("0,3,6", 2020)
       436
   """
-  def execute(input) do
-    #
+  def execute(input, target) do
     [turn, spoken_at, turn_to_spoken] = initialize(input)
-
-    # IO.puts(
-    #   "turn: #{turn}, sa: #{inspect(spoken_at, pretty: true)}, turn_to_spoken: #{
-    #     inspect(turn_to_spoken, pretty: true)
-    #   }"
-    # )
-
-    spoken_at_2020(turn, spoken_at, turn_to_spoken)
+    spoken_at_target(turn, spoken_at, turn_to_spoken, target)
   end
 
   @doc """
-      # iex> Aoc2020.Day15.Part1.initialize("0,3,6")
+      # iex> Aoc2020.Day15.initialize("0,3,6")
       # [4, %{0 => [1], 3 => [2], 6 => [3]}, %{1 => 0, 2 => 3, 3 => 6}]
 
-      iex> Aoc2020.Day15.Part1.initialize("1,3,2")
+      iex> Aoc2020.Day15.initialize("1,3,2")
       [4, %{1 => [1], 3 => [2], 2 => [3]}, %{1 => 1, 2 => 3, 3 => 2}]
   """
   def initialize(nums) do
@@ -45,39 +37,30 @@ defmodule Aoc2020.Day15.Part1 do
     [turn + 1, spoken_at, turn_to_spoken]
   end
 
-  # turn: 4, sa: %{1 => [0], 2 => [3], 3 => [6]}, turn_to_spoken: %{1 => 0, 2 => 3, 3 => 6}
   @doc """
-      iex> Aoc2020.Day15.Part1.spoken_at_2020(4, %{0 => [1], 3 => [2], 6 => [3]},
-      ...>    %{1 => 0, 2 => 3, 3 => 6})
+      iex> Aoc2020.Day15.spoken_at_target(4, %{0 => [1], 3 => [2], 6 => [3]},
+      ...>    %{1 => 0, 2 => 3, 3 => 6}, 2020)
       436
 
-      iex> Aoc2020.Day15.Part1.spoken_at_2020(4, %{1 => [1], 3 => [2], 2 => [3]},
-      ...>    %{1 => 1, 2 => 3, 3 => 2})
+      iex> Aoc2020.Day15.spoken_at_target(4, %{1 => [1], 3 => [2], 2 => [3]},
+      ...>    %{1 => 1, 2 => 3, 3 => 2}, 2020)
       1
 
-      iex> Aoc2020.Day15.Part1.spoken_at_2020(4, %{3 => [1], 1 => [2], 2 => [3]},
-      ...>  %{1 => 3, 2 => 1, 3 => 2})
+      iex> Aoc2020.Day15.spoken_at_target(4, %{3 => [1], 1 => [2], 2 => [3]},
+      ...>  %{1 => 3, 2 => 1, 3 => 2}, 2020)
       1836
   """
-  def spoken_at_2020(turn, last_two_turns_spoken, turn_to_spoken) do
+  def spoken_at_target(turn, last_two_turns_spoken, turn_to_spoken, target) do
     # last_two_turns_spoken %{n => [t0, t1]} -> "When where the last two times n was used?"
     # turn_to_spoken %{t => n} -> "Which number was used on turn t?"
 
     # get last number used
     last_num_used = Map.get(turn_to_spoken, turn - 1)
 
-    if turn == 2021 do
+    if turn == target + 1 do
       last_num_used
     else
       last_two_turns_num_was_used = Map.get(last_two_turns_spoken, last_num_used)
-
-      # TODO(gianluca): Remove when done
-      # TODO(gianluca): Remove when done
-      # IO.puts(
-      #   "turn: #{turn}, last_num_used: #{last_num_used}, last_two_turns_num_was_used: #{
-      #     inspect(last_two_turns_num_was_used, pretty: true)
-      #   }"
-      # )
 
       cond do
         last_two_turns_num_was_used == nil || length(last_two_turns_num_was_used) == 1 ->
@@ -86,19 +69,15 @@ defmodule Aoc2020.Day15.Part1 do
           times_zero_used = Map.get(last_two_turns_spoken, 0, [])
           last_two_times = add_to_overflow(times_zero_used, turn)
 
-          spoken_at_2020(
+          spoken_at_target(
             turn + 1,
             Map.put(last_two_turns_spoken, 0, last_two_times),
-            Map.put(turn_to_spoken, turn, 0)
+            Map.put(turn_to_spoken, turn, 0),
+            target
           )
 
         length(last_two_turns_num_was_used) == 2 ->
           [t0, t1] = last_two_turns_num_was_used
-          # TODO(gianluca): Remove when done
-          # IO.puts("t0: #{t0}")
-          # # TODO(gianluca): Remove when done
-          # IO.puts("t1: #{t1}")
-          # # used before at least once
 
           # if used twice -> overflow
           # Last two turns the number was spoken was
@@ -107,10 +86,11 @@ defmodule Aoc2020.Day15.Part1 do
           # same as times_t1_used!
           overflow_list = add_to_overflow(times_t0_used, turn)
 
-          spoken_at_2020(
+          spoken_at_target(
             turn + 1,
             Map.put(last_two_turns_spoken, num_to_speak, overflow_list),
-            Map.put(turn_to_spoken, turn, num_to_speak)
+            Map.put(turn_to_spoken, turn, num_to_speak),
+            target
           )
 
         true ->
@@ -120,13 +100,13 @@ defmodule Aoc2020.Day15.Part1 do
   end
 
   @doc """
-      iex> Aoc2020.Day15.Part1.add_to_overflow([1], 3)
+      iex> Aoc2020.Day15.add_to_overflow([1], 3)
       [1, 3]
 
-      iex> Aoc2020.Day15.Part1.add_to_overflow([], 3)
+      iex> Aoc2020.Day15.add_to_overflow([], 3)
       [3]
 
-      iex> Aoc2020.Day15.Part1.add_to_overflow([1, 2], 3)
+      iex> Aoc2020.Day15.add_to_overflow([1, 2], 3)
       [2, 3]
   """
   def add_to_overflow([_t0, t1], el) do
---
+++ test/day15_test.exs
@@ -0,0 +1,15 @@
+defmodule Day15Test do
+  use ExUnit.Case
+  doctest Aoc2020.Day15
+  import Aoc2020.Day15
+
+  test "part 1" do
+    assert solve() == 206
+  end
+
+  # @tag timeout: :infinity
+  @tag :skip
+  test "part 2" do
+    assert solve(30_000_000) == 955
+  end
+end
