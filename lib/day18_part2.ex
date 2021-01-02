920e741ad21561bbbb0a129ace30cbce522d7d69
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2021-01-02 08:51:12 +0200

    Finish day 18

--- lib/day18_part2.ex
+++ lib/day18_part2.ex
@@ -19,5 +19,86 @@ defmodule Aoc2020.Day18.Part2 do
   end
 
   def part_two_solver(lines) do
+    res =
+      lines
+      |> Enum.reduce(0, fn line, acc ->
+        acc + parse_line(line)
+      end)
+
+    IO.puts("res: #{res}")
+    res
+  end
+
+  @doc """
+  Strategy: If we had no parens, we can use evaluate_group directly and be done with it.
+  With parens, we need to evaluate the innermost group, and go up until we get back to
+  a sequence without parens.
+  """
+  def parse_line(l) do
+    last_group =
+      l
+      |> String.split(~r/[+*()]/, trim: true, include_captures: true)
+      |> Enum.map(&String.trim/1)
+      |> Enum.filter(&(&1 != ""))
+      |> Enum.reduce([], fn token, acc ->
+        cond do
+          String.match?(token, ~r/[0-9]+/) ->
+            [String.to_integer(token) | acc]
+
+          String.match?(token, ~r/\)/) ->
+            idx = Enum.find_index(acc, &(&1 == "("))
+            {group, rest} = Enum.split(acc, idx + 1)
+
+            # IO.puts("group: #{inspect(group, pretty: true, charlists: :as_list)}")
+            # IO.puts("rest: #{inspect(rest, pretty: true, charlists: :as_list)}")
+
+            res = evaluate_group(Enum.reverse(group -- [List.last(group)]))
+
+            # x = [res | rest]
+
+            # IO.puts(
+            #   "matched ')', group evaluated: #{inspect(x, pretty: true, charlists: :as_list)}"
+            # )
+
+            # x
+            [res | rest]
+
+          true ->
+            [token | acc]
+        end
+      end)
+
+    # IO.puts("last_group: #{inspect(last_group, pretty: true, charlists: :as_list)}")
+
+    evaluate_group(Enum.reverse(last_group))
+  end
+
+  @doc """
+  Evaluate unparenthesized expression. Important: there can't be parentheses!
+  """
+  def evaluate_group([x, "+", y | rest]) do
+    # IO.puts("x: #{x}")
+    # IO.puts("rest: #{inspect(rest, pretty: true, charlists: :as_list)}")
+
+    evaluate_group([x + y] ++ rest)
+  end
+
+  def evaluate_group([x, "*" | rest]) do
+    # IO.puts("x: #{x}")
+    # IO.puts("rest: #{inspect(rest, pretty: true, charlists: :as_list)}")
+
+    x * evaluate_group(rest)
+  end
+
+  def evaluate_group([u]), do: u
+
+  def do_op(a, b, op) do
+    case op do
+      "+" ->
+        a + b
+
+      "*" ->
+        a * b
+    end
   end
 end
--- test/day18_part2_test.exs
+++ test/day18_part2_test.exs
@@ -3,7 +3,24 @@ defmodule Day18Part2Test do
   doctest Aoc2020.Day18.Part2
   import Aoc2020.Day18.Part2
 
-  test "part 2" do
-    assert solve() == nil
+  test "examples" do
+    test_example("1 + 2 * 3 + 4 * 5 + 6", 231)
+    test_example("1 + (2 * 3) + (4 * (5 + 6))", 51)
+    test_example("2 * 3 + (4 * 5)", 46)
+    test_example("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)
+    test_example("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669_060)
+    test_example("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)
+  end
+
+  @tag :skip
+  test "evaluate_group" do
+    assert evaluate_group([1, "+", 3]) == 4
+    assert evaluate_group([1, "*", 3]) == 3
+    assert evaluate_group([4, "*", 3, "+", 2]) == 20
+    assert evaluate_group([4, "*", 3, "*", 2, "+", 5]) == 84
+  end
+
+  defp test_example(line, expected) do
+    assert parse_line(line) == expected
   end
 end
